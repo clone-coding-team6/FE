cdad167adecf5dbd606af11be00f652255a9552c
leejuae1020 <leejuae1020@naver.com>
2023-03-05 18:45:54 +0900

    작업확인을 위한 주석

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Main from "./pages/Main.js";
 import MyProfile from "./pages/MyProfile";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import MypageLayout from "./components/Layout/MypageLayout.jsx";
 
 
 
 const App = () => {
   return (
     
-        <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/Main" element={<Main />} />
-              <Route path="/MyProfile" element={ <MyProfile />} />
-              <Route path="/SignUp" element={<SignUp />} />
-            </Routes>
-        </BrowserRouter>
+        // <BrowserRouter>
+        //     <Routes>
+        //       <Route path="/" element={<Login />} />
+        //       <Route path="/Main" element={<Main />} />
+        //       <Route path="/MyProfile" element={ <MyProfile />} />
+        //       <Route path="/SignUp" element={<SignUp />} />
+        //     </Routes>
+        // </BrowserRouter>
+
+        <MypageLayout/>
     
      
     
