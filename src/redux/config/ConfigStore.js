8776666a0bb37d6cf860a4dd790071742fbeccf9
leejuae1020 <leejuae1020@naver.com>
2023-03-08 15:25:14 +0900

    수정

--- src/redux/config/ConfigStore.js
+++ src/redux/config/ConfigStore.js
@@ -1,7 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
-import articleSlice from '../modules/articleSlice';
-import userSlice from '../modules/userSlice';
-import commentSlice from '../modules/commentSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import articleSlice from "../modules/articleSlice";
+import userSlice from "../modules/userSlice";
+import commentSlice from "../modules/commentSlice";
 
 export const store = configureStore({
   reducer: {
